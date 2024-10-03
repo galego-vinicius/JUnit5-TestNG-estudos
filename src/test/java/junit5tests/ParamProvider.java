3e297eac70a7633085941392a668c2169152c491
galego-vinicius <vfelix1998@gmail.com>
2024-10-03 11:19:13 -0300

    refact: Parameterized Tests with @MethodSouce

--- src/test/java/junit5tests/ParamProvider.java
+++ src/test/java/junit5tests/ParamProvider.java
@@ -1,14 +1,32 @@
 package junit5tests;
 
 import org.junit.jupiter.params.provider.Arguments;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 
 public class ParamProvider {
 
     // Classe de teste estiver em classe separada de Classe de método -> utilizar o "static"
+
+    static List<String> sourceString() {
+        //processing done here
+        return Arrays.asList("tomato", "carrot", "cabbage");
+    }
+
+    static Stream<String> sourceStringAsStream(){
+        return  Stream.of("beetroot", "apple", "apple");
+    }
+
+    static List<Arguments> sourceList_StringDouble(){
+        //processing
+        return Arrays.asList(arguments("tomato",2.0),
+                arguments("carrot", 4.5),
+                arguments("cabbage", 7.8));
+    }
+
     static Stream<Arguments> sourceStream_StringDouble(){
         return  Stream.of(arguments("apple", 8.9),
                 arguments("pear", 1.9));
--- src/test/java/junit5tests/ParameterizedTests.java
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -3,10 +3,6 @@ package junit5tests;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ParameterizedTests {
@@ -66,41 +62,25 @@ public class ParameterizedTests {
 // ---------------------------------------------------------------------------------------------------------------- //
 
     @ParameterizedTest
-    @MethodSource(value = "sourceString")
+    @MethodSource(value = "junit5tests.ParamProvider#sourceString")
     void methodSource_String(String param1){
         System.out.println("param1 = " + param1);
     }
 
-    List<String> sourceString() {
-        //processing done here
-    return Arrays.asList("tomato", "carrot", "cabbage");
-    }
-
 // ---------------------------------------------------------------------------------------------------------------- //
 
     @ParameterizedTest
-    @MethodSource(value = "sourceStringAsStream")
+    @MethodSource(value = "junit5tests.ParamProvider#sourceStringAsStream")
     void methodSource_StringStream(String param1){
         System.out.println("param1 = " + param1);
     }
 
-    Stream<String> sourceStringAsStream(){
-        return  Stream.of("beetroot", "apple", "apple");
-    }
-
 // ---------------------------------------------------------------------------------------------------------------- //
 
     @ParameterizedTest
-    @MethodSource(value = "sourceList_StringDouble")
+    @MethodSource(value = "junit5tests.ParamProvider#sourceList_StringDouble")
     void methodSource_StringDoubleList(String param1, double param2){
         System.out.println("param1 = " + param1 + ", param2 = " + param2);
-    }
-
-    List<Arguments> sourceList_StringDouble(){
-        //processing
-        return Arrays.asList(arguments("tomato",2.0),
-                arguments("carrot", 4.5),
-                arguments("cabbage", 7.8));
     }
 
 // ---------------------------------------------------------------------------------------------------------------- //
