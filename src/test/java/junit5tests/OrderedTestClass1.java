d7d1830394a962637e0fe23886baaca810d6675c
galego-vinicius <vfelix1998@gmail.com>
2024-10-03 11:40:50 -0300

    add: Test Run Order

---
+++ src/test/java/junit5tests/OrderedTestClass1.java
@@ -0,0 +1,47 @@
+package junit5tests;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class) // Ordena em ordem alfanumerico
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) //Colocando esta anotação, não é necessario utilizar "static" no BeforeAll e BeforeEach
+public class OrderedTestClass1 {
+
+    @BeforeAll //Primeiro de toda a classe
+    void beforeAll(){
+        System.out.println("--This is the before All method");
+    }
+
+    @BeforeEach //Primeiro de toda o método
+    void beforeEach(){
+        System.out.println("----This i the before each method");
+    }
+
+    @AfterAll //Ultimo de toda a classe
+    void afterAll(){
+        System.out.println("--This is the after All method");
+    }
+
+    @AfterEach //Ultimo de to-do metodo
+    void afterEach(){
+        System.out.println("----This is the after each method");
+    }
+
+    @Test
+    // acessModifier returnType nameOfMethod (params){}
+    void firstMethod(){
+        System.out.println("This is the first test method");
+    }
+
+    @Test
+    @DisplayName("US1 - TC1 - this method is the second one")
+    void secondMethod(){
+        System.out.println("This is the second test method");
+    }
+
+    @Test
+    @DisplayName("A display name")
+    void thirdTest(){
+        System.out.println("This is the third test method");
+    }
+
+}
