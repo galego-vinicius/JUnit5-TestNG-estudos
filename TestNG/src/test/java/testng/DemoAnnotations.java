d62bb769fe907be6f67d72df1f1acfdd4d314d7f
galego-vinicius <vfelix1998@gmail.com>
2024-10-05 18:39:08 -0300

    feat: testNG

---
+++ TestNG/src/test/java/testng/DemoAnnotations.java
@@ -0,0 +1,19 @@
+package testng;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DemoAnnotations {
+    // @BeforeSuite, @AfterSuite, @BeforeTest, @AfterTest
+    // @BeforeClass, @AfterClass, @BeforeMethod, @AfterMethod
+
+    @Test(priority = 1)
+    public void setUp(){
+    }
+
+    // assertTrue, assertFalse, assertSame, assertNotSame
+    // assertNotNull, assertEquals
+
+    // Hard assertions and Soft assertions
+
+}
